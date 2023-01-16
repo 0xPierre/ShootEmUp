c533e43cae04578f60cf5e8988453492e4c3f6dd
Binasta <bastian.beaufils44@gmail.com>
2023-01-16 16:39:27 +0100

    render enemy

--- ShootEmUp/Enemy.c
+++ ShootEmUp/Enemy.c
@@ -43,10 +43,28 @@ void Enemy_Update(Enemy *self)
 
 void Enemy_Render(Enemy *self)
 {
-
+    // On récupère des infos essentielles (communes à tout objet)
+    Scene* scene = self->scene;
+    SDL_Renderer* renderer = Scene_GetRenderer(scene);
+    Assets* assets = Scene_GetAssets(scene);
+    Camera* camera = Scene_GetCamera(scene);
+    // On calcule la position en pixels en fonction de la position
+    // en tuiles, la taille de la fenêtre et la taille des textures.
+    float scale = Camera_GetWorldToViewScale(camera);
+    SDL_FRect dst = { 0 };
+    // Changez 48 par une autre valeur pour grossir ou réduire l'objet
+    dst.h = 48 * PIX_TO_WORLD * scale;
+    dst.w = 48 * PIX_TO_WORLD * scale;
+    Camera_WorldToView(camera, self->position, &dst.x, &dst.y);
+    // Le point de référence est le centre de l'objet
+    dst.x -= 0.50f * dst.w;
+    dst.y -= 0.50f * dst.h;
+    // On affiche en position dst (unités en pixels)
+    SDL_RenderCopyExF(
+        renderer, self->texture, NULL, &dst, 90.0f, NULL, 0);
 }
 
 void Enemy_Damage(Enemy *self, int damage)
 {
-
+    
 }
